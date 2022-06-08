6673efd5c45fc28baaa1e0893555289c7b3e87a8
Keshav Khatri <104303172+phoenixkeshav119@users.noreply.github.com>
2022-06-08 19:23:51 +0545

    banner in progress

--- pages/home/banner.php
+++ pages/home/banner.php
@@ -1,16 +1,26 @@
-<div class="banner">
-  <!-- Hero Banner Section -->
-<section class="hero-banner bg-light py-5">
-    <div class="container">
-        <div class="row row align-items-center">
-            <div class="col-lg-5 offset-lg-1 order-lg-1">
-                <img src="web-development-vector.png" class="img-fluid" alt="Web Development">
-            </div>
-            <div class="col-lg-6">
-                <h1 class="mt-3">Web Designing & Development</h1>
-                <p class="lead text-secondary my-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-            </div>
-        </div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/banner-img.jpg" alt="First slide">
     </div>
-</section>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/device-img.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/device-img.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
