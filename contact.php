4b53246b1026cf14eec1a82942cbda48cf35af91
Keshav Khatri <104303172+phoenixkeshav119@users.noreply.github.com>
2022-06-14 18:54:47 +0545

    contact page updated

--- contact.php
+++ contact.php
@@ -3,11 +3,12 @@
 <title>Nepal Voting Machine : Contact</title>
 <?php include_once("includes/meta.php") ?>
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
 
 <body>
   <?php include_once("includes/topnav.php") ?>
   <?php include_once("includes/navigation.php") ?>
+
   <div class="container">
     <?php include_once("pages/contact/contact.php") ?>
 
--- pages/contact/contact.php
+++ pages/contact/contact.php
@@ -26,31 +26,25 @@
                     </div>
                 </div>
 
-                <div class="col-md-5">
-                    <label for="validationCustom04" class="form-label">Province</label>
-                    <select class="form-select" id="validationCustom04" required>
-                        <option selected disabled value="">Choose...</option>
-                        <option>Province 1</option>
-                        <option>Province 2</option>
-                        <option>Province 3</option>
-                        <option>Province 4</option>
-                        <option>Province 5</option>
-                        <option>Province 6</option>
-                        <option>Province 7</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please select a valid Province.
-                    </div>
-                </div>
-
                 <div class="col-md-6">
-                    <label for="validationCustom01" class="form-label">Address</label>
+                    <label for="validationCustom01" class="form-label">Phone No.</label>
                     <input type="text" class="form-control" id="validationCustom01" placeholder="Enter your current address" required>
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                     <div class="invalid-feedback">
-                        Please provide a valid address.
+                        Please provide a valid phone number.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="validationCustom01" class="form-label">Message</label>
+                    <textarea type="text" class="form-control" id="validationCustom01" placeholder="Message" required></textarea>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                        Please type some message text.
                     </div>
                 </div>
 
@@ -73,11 +67,22 @@
             </form>
         </div>
         <div class="col md-6">
-            <div id="map-container-google-1" class="z-depth-1-half map-container">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.856947277897!2d85.32059501500201!3d27.721702782785922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb191a6b63303b%3A0xff246294118c21e6!2sRamlaxman%20Innovations!5e0!3m2!1sen!2snp!4v1655198541718!5m2!1sen!2snp" width="auto" height="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="contact-detail">
+                <img src="img/ramlaxman-innovation.png" width="20%" alt="ramlaxman-innovation-logo">
+                <div class="contact-para">
+                    <p>An ISO 9001:2016 Certified & Multiple IP Holder Company</p>
+                    <p>Tel: 00977-1-4444576, 4416900</p>
+                    <p>Fax: 00977-1-4444575</p>
+                    <p>E-mail: info@technosales.com.np, community_laxman@yahoo.com</p>
+                    <p>Lazimpat, Kathmandu</p>
+                </div>
             </div>
         </div>
 
-        
+
+    </div>
+
+    <div id="map-container-google-1" class="z-depth-1-half map-container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.856947277897!2d85.32059501500201!3d27.721702782785922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb191a6b63303b%3A0xff246294118c21e6!2sRamlaxman%20Innovations!5e0!3m2!1sen!2snp!4v1655198541718!5m2!1sen!2snp" width="auto" height="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
