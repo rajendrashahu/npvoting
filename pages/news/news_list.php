93a667f8e17e4fc8bec409d8122fbfaef939d318
Rajendra Shahu <rjn.s2009@gmail.com>
2022-06-14 17:11:39 +0545

    news and favicon added

--- pages/news/news_list.php
+++ pages/news/news_list.php
@@ -9,7 +9,6 @@
                     <h6 class="subtitle font-weight-normal">You can relay on our amazing features list and also our customer services will be great experience for you without doubt</h6>
                 </div>
                 <!-- Column -->
-                <!-- Column -->
             </div>
             <div class="row mt-4">
                 <!-- Column -->
@@ -18,7 +17,6 @@
                         <a href="https://english.headlinenepal.com/details/17020" target="_blank"><img class="card-img-top" src="img/news-one.jpg" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Nov<span class="d-block">28</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://english.headlinenepal.com/details/17020" target="_blank" class="text-decoration-none link">UML General Convention Election Used 100 Voting Machine Above 5 Million</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://english.headlinenepal.com/details/17020" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
@@ -28,7 +26,6 @@
                         <a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference" target="_blank"><img class="card-img-top" src="img/news-two.jpg" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Oct<span class="d-block">30</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference" target="_blank" class="text-decoration-none link">UML’s decision to use Nepal-made electronic voting machines</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
@@ -38,18 +35,7 @@
                         <a href="https://www.southasiatime.com/2019/10/16/nrna-to-use-electronic-voting-machine-made-in-nepal" target="_blank"><img class="card-img-top" src="img/news-three.jpeg" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Oct<span class="d-block">16</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://www.southasiatime.com/2019/10/16/nrna-to-use-electronic-voting-machine-made-in-nepal" target="_blank" class="text-decoration-none link">NRNA to use electronic voting machine made in Nepal</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://www.southasiatime.com/2019/10/16/nrna-to-use-electronic-voting-machine-made-in-nepal" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-4 on-hover">
-                    <div class="card border-0 mb-4">
-                        <a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference" target="_blank"><img class="card-img-top" src="img/cpn-uml.png" alt="device image"></a>
-                        <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Oct<span class="d-block">30</span></div>
-                        <h5 class="font-weight-medium mt-3"><a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference/" target="_blank" class="text-decoration-none link">UML’s decision to use Nepal-made electronic voting machines in general conference</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
-                        <a href="https://himalsanchar.com/umls-decision-to-use-nepal-made-electronic-voting-machines-in-general-conference/" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
                 <!-- Column -->
@@ -58,17 +44,15 @@
                         <a href="https://en.setopati.com/political/157476" target="_blank"><img class="card-img-top" src="img/voting-uml.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Nov<span class="d-block">30</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://en.setopati.com/political/157476" target="_blank" class="text-decoration-none link">Voting for UML leadership concludes, results within 3 hours</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://en.setopati.com/political/157476" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
                 <!-- Column -->
                 <div class="col-md-4 on-hover">
                     <div class="card border-0 mb-4">
-                        <a href="https://nepaliheadlines.com/demand-use-electronic-voting-machine-made-nepal/" target="_blank"><img class="card-img-top" src="img/nepali-voting.png" alt="device image"></a>
+                        <a href="https://nepaliheadlines.com/demand-use-electronic-voting-machine-made-nepal/" target="_blank"><img class="card-img-top" src="img/cpn-uml.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Feb<span class="d-block">8</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://nepaliheadlines.com/demand-use-electronic-voting-machine-made-nepal/" target="_blank" class="text-decoration-none link">DEMAND TO USE ELECTRONIC VOTING MACHINE MADE IN NEPAL</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://nepaliheadlines.com/demand-use-electronic-voting-machine-made-nepal/" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
@@ -78,7 +62,6 @@
                         <a href="https://www.setopati.com/archive-news/52556" target="_blank"><img class="card-img-top" src="img/nepali-voting.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Sep<span class="d-block">1</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://www.setopati.com/archive-news/52556" target="_blank" class="text-decoration-none link">भोटिङ मेसिनमा फैलाइएको भ्रम र यथार्थ</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://www.setopati.com/archive-news/52556" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
@@ -88,7 +71,6 @@
                         <a href="https://clickmandu.com/2017/11/35210.html" target="_blank"><img class="card-img-top" src="img/voting-no-permission.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Nov<span class="d-block">3</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://clickmandu.com/2017/11/35210.html" target="_blank" class="text-decoration-none link">नेपालीले बनाएको भोटिङ मेसिनले निर्वाचन आयोगबाट अझै अनुमति पाएन</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://clickmandu.com/2017/11/35210.html" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
@@ -98,18 +80,65 @@
                         <a href="https://www.setoghar.com/archives/59378" target="_blank"><img class="card-img-top" src="img/voting-machine.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Nov<span class="d-block">3</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://www.setoghar.com/archives/59378" target="_blank" class="text-decoration-none link">प्रविधिको विकासमा दाजुभाइकाे समर्पण, कम्युनिटी साइरन देखि भाेटिङ मेसिन सम्म, अबका के चाहन्छन् रामलक्ष्मण ?</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://www.setoghar.com/archives/59378" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
                 <!-- Column -->
                 <div class="col-md-4 on-hover">
                     <div class="card border-0 mb-4">
-                        <a href="https://www.lokpath.com/story/53783" target="_blank"><img class="card-img-top img-responsive" src="img/rm-voting-machine.png" alt="device image"></a>
+                        <a href="https://www.lokpath.com/story/53783" target="_blank"><img class="card-img-top" src="img/rm-voting-machine.png" alt="device image"></a>
                         <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Aug<span class="d-block">12</span></div>
                         <h5 class="font-weight-medium mt-3"><a href="https://www.lokpath.com/story/53783" target="_blank" class="text-decoration-none link">पजुम्ल्याहा युवाको प्राविधिक जादु : बिजुलीको स्वचालित स्वीचदेखि भोटिङ मेसिनसम्म</a></h5>
-                        <p class="mt-3">Kathmandu, Nepal</p>
                         <a href="https://www.lokpath.com/story/53783" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <a href="https://myrepublica.nagariknetwork.com/news/developers-of-nepali-voting-machine-feel-discouraged/" target="_blank"><img class="card-img-top" src="img/developer-nepal-voting.png" alt="device image"></a>
+                        <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Aug<span class="d-block">12</span></div>
+                        <h5 class="font-weight-medium mt-3"><a href="https://myrepublica.nagariknetwork.com/news/developers-of-nepali-voting-machine-feel-discouraged/" target="_blank" class="text-decoration-none link">Developers of Nepali voting machine feel discouraged</a></h5>
+                        <a href="https://myrepublica.nagariknetwork.com/news/developers-of-nepali-voting-machine-feel-discouraged/" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <iframe width="416" height="288" src="https://www.youtube.com/embed/njxfYCmraxg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h5 class="font-weight-medium mt-3"><a href="https://www.youtube.com/watch?app=desktop&v=njxfYCmraxg" target="_blank" class="text-decoration-none link">यस्तो छ, नेपालमै बनेको भोटिङ मेशिन || Nepal's Voting Machine Developed By Ram Laxman Rimal</a></h5>
+                        <a href="https://www.youtube.com/watch?app=desktop&v=njxfYCmraxg" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <iframe width="416" height="288" src="https://www.youtube.com/embed/2DyPEVj9ZpI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h5 class="font-weight-medium mt-3"><a href="https://www.youtube.com/watch?app=desktop&v=2DyPEVj9ZpI" target="_blank" class="text-decoration-none link">ramlaxman rimal</a></h5>
+                        <a href="https://www.youtube.com/watch?app=desktop&v=2DyPEVj9ZpI" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <iframe width="416" height="288" src="https://www.youtube.com/embed/Ur-xtR9ATYs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h5 class="font-weight-medium mt-3"><a href="https://www.youtube.com/watch?app=desktop&v=Ur-xtR9ATYs&t" target="_blank" class="text-decoration-none link">डिजिटल भोटिङ मेसिनले कसरी काम गर्छ ?</a></h5>
+                        <a href="https://www.youtube.com/watch?app=desktop&v=Ur-xtR9ATYs&t" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <iframe width="416" height="288" src="https://www.youtube.com/embed/BqNhNutoE1M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h5 class="font-weight-medium mt-3"><a href="https://www.youtube.com/watch?app=desktop&v=BqNhNutoE1M" target="_blank" class="text-decoration-none link">Nepali Voting machine - Old Version</a></h5>
+                        <a href="https://www.youtube.com/watch?app=desktop&v=BqNhNutoE1M" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-4 on-hover">
+                    <div class="card border-0 mb-4">
+                        <iframe width="416" height="288" src="https://www.youtube.com/embed/a_HrBLhwu8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h5 class="font-weight-medium mt-3"><a href="https://www.youtube.com/watch?v=a_HrBLhwu8Y" target="_blank" class="text-decoration-none link">How easy is Nepali Voting machine</a></h5>
+                        <a href="https://www.youtube.com/watch?v=a_HrBLhwu8Y" target="_blank" class="text-decoration-none linking text-themecolor mt-2">read more &nbsp; &raquo;</a>
                     </div>
                 </div>
 
