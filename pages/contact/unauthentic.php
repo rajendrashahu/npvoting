11c0d40fc228202389ab16789c5c56ee642ee0cc
Keshav Khatri <104303172+phoenixkeshav119@users.noreply.github.com>
2022-06-15 19:30:40 +0545

    icon added

--- pages/contact/unauthentic.php
+++ pages/contact/unauthentic.php
@@ -1,10 +1,14 @@
 <?php
-if ($_GET['return'] = 'invalid'){ ?>
+if ($_GET['return'] = 'invalid') { ?>
     <div class="contact">
-        <h2>Error ! unauthentic.</h2>
+        <!-- Warning Alert -->
+        <div class="alert alert-warning alert-dismissible d-flex align-items-center fade show">
+            <i class="bi-exclamation-triangle-fill"></i>
+            <strong class="mx-2">Warning!</strong> There was a problem with your network connection.
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
     </div>
-<?php }
-else{
+<?php } else {
     header("Location: https://www.pioneersoftech.com/npvoting");
     exit();
 }
