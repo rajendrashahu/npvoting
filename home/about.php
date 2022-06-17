3d7e12af50cfaab20df3611d9cd724719f127930
Keshav Khatri <104303172+phoenixkeshav119@users.noreply.github.com>
2022-06-17 12:25:48 +0545

    vote count added

--- home/about.php
+++ home/about.php
@@ -2,26 +2,43 @@
     <h2>-ABOUT US-</h2>
     <div>
         <div>
-        <h3>background</h3>
-        <p>Citizens' right to vote and elect their representative in a periodic election is a basic
-            requirement for any democratic nation. In a developing country like Nepal, unfair election practices are speculated most of the time and rightly so with our outdated electoral procedures. Invalid votes also play a major role in the somewhat unpredictable final result. While 3% average invalid votes are expected internationally, during the 2017 national elections, it reached 17% in Lalitpur, the first fully literate district in Nepal, and peaked as high as 22% outside the Kathmandu Valley. Such a high number of null votes is because of a complex electoral system and lack of voter education. With a whopping 125 political parties and their similar-looking symbols, it is a very confusing experience for the average Nepali to begin with. A host of other restrictions like touching the border with a stamp, smudging the stamp, or even touching another symbol when it is wet or wrongly folding the ballot paper, will surely confuse even the well-educated.
+            <h3>background</h3>
+            <p>Citizens' right to vote and elect their representative in a periodic election is a basic
+                requirement for any democratic nation. In a developing country like Nepal, unfair election practices are speculated most of the time and rightly so with our outdated electoral procedures. Invalid votes also play a major role in the somewhat unpredictable final result. While 3% average invalid votes are expected internationally, during the 2017 national elections, it reached 17% in Lalitpur, the first fully literate district in Nepal, and peaked as high as 22% outside the Kathmandu Valley. Such a high number of null votes is because of a complex electoral system and lack of voter education. With a whopping 125 political parties and their similar-looking symbols, it is a very confusing experience for the average Nepali to begin with. A host of other restrictions like touching the border with a stamp, smudging the stamp, or even touching another symbol when it is wet or wrongly folding the ballot paper, will surely confuse even the well-educated.
 
-            Nepal ranks among the poorest countries in the world; however, it spends large sums of investment to ensure fair elections. According to the study by Election Observation Committee, Nepal (EOC-Nepal), the government and the candidates spent a combined total of <b>Rs 131.63 billion</b> on elections in 2017. The Nepali government's reluctance to adopt modern technology costs billions in elections when a little foresight can radically curtail these expenses.
+                Nepal ranks among the poorest countries in the world; however, it spends large sums of investment to ensure fair elections. According to the study by Election Observation Committee, Nepal (EOC-Nepal), the government and the candidates spent a combined total of <b>Rs 131.63 billion</b> on elections in 2017. The Nepali government's reluctance to adopt modern technology costs billions in elections when a little foresight can radically curtail these expenses.
 
-            After ruminating over innovative ideas to improve the current electoral process, Ram Laxman Innovations developed a voting machine to make elections error-proof by embracing modern technology. This machine is locally designed and produced, bearing in mind the exact needs of the people and the country.
-        </p>
+                After ruminating over innovative ideas to improve the current electoral process, Ram Laxman Innovations developed a voting machine to make elections error-proof by embracing modern technology. This machine is locally designed and produced, bearing in mind the exact needs of the people and the country.
+            </p>
         </div>
-
+        <div class="voting-section">
+            <div class="vote-count">
+                <div class="count-one">
+                    <h5>Vote Count</h5>
+                    <div class="vote-num">
+                        <p>223344</p>
+                    </div>
+                </div>
+            </div>
+            <div class="vote-count">
+                <div class="count-two">
+                    <h5>Vote Count</h5>
+                    <div class="vote-num">
+                        <p>223344</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div>
-        <h3>how it works</h3>
-        <p>
-            RL voting machine is a modern technology with a large touch screen that replicates the current paper ballot to avoid (culture shock). For further convenience of voters, voice assistance is available to guide them through the entire process. It is a versatile machine and supports even the picture and names of the candidates and political parties. We have added verification options to rectify mistakes in the system which has proven very useful for first-time users.
-        </p>
-        <p>
-            The voting machine is equipped with a 20 Wh battery that provides an uninterrupted power supply in case of a power outage. The election can go smoothly for a whole day with 12 hours of power backup. Moreover, its' charging mechanism has been designed, keeping Nepali geographical diversity into consideration. The voting machine can be charged with solar or batteries from motorbikes and other vehicles in rural places without electricity.
-        </p>
+            <h3>how it works</h3>
+            <p>
+                RL voting machine is a modern technology with a large touch screen that replicates the current paper ballot to avoid (culture shock). For further convenience of voters, voice assistance is available to guide them through the entire process. It is a versatile machine and supports even the picture and names of the candidates and political parties. We have added verification options to rectify mistakes in the system which has proven very useful for first-time users.
+            </p>
+            <p>
+                The voting machine is equipped with a 20 Wh battery that provides an uninterrupted power supply in case of a power outage. The election can go smoothly for a whole day with 12 hours of power backup. Moreover, its' charging mechanism has been designed, keeping Nepali geographical diversity into consideration. The voting machine can be charged with solar or batteries from motorbikes and other vehicles in rural places without electricity.
+            </p>
         </div>
-        
+
 
     </div>
     <div class="readmore-btn">
